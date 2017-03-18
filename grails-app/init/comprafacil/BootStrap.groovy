711de8d73c96a228e9e9cc6f9d81ab30c5d0f961
victorsantiago1708 <victorsantiago1708@gmail.com>
2017-03-18 09:13:21 -0300

    corrigindo erro na criação do usuário padrão ADMIN

--- grails-app/init/comprafacil/BootStrap.groovy
+++ grails-app/init/comprafacil/BootStrap.groovy
@@ -4,7 +4,12 @@ class BootStrap {
 
     def init = { servletContext ->
         def adminRole = Role.findOrSaveByAuthority('ROLE_ADMIN')
-        def user = Usuario.findOrSaveByUsernameAndPassword('admin', 'admin')
+        def user = Usuario.findByUsername('admin')
+
+        if(!user){
+            user = new Usuario(username: 'admin', password: 'admin')
+            user.save(failOnError: true, flush: true)
+        }
 
         if(!user.authorities.contains(adminRole)){
             UsuarioRole.create(user, adminRole)
